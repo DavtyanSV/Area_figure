f61f6a4aa659f0bb084ec0c6884c4319b88f7c2a
Suren <davtyan_sv@mail.ru>
2023-08-14 16:41:51 +0400

    Добавил юнит тесты, проверку треугольника и площадь у неизвестной фигуры

--- Class1.cs
+++ Class1.cs
@@ -1,8 +1,10 @@
 ﻿namespace Area_figure
 {
-    public class Circle_Area
+
+    public class FigureCalculator
     {
-        public double CalculateArea(double radius)
+        
+        public double CalculateAreaCircle(double radius)
         {
             if (radius <= 0)
             {
@@ -12,11 +14,19 @@
             double area = Math.PI * Math.Pow(radius, 2);
             return area;
         }
-    }
 
-    public class Triangle_Area
-    {
-        public double CalculateArea(double a, double b, double c)
+        public double CalculateUnknownShapeArea(double[] sides)
+        {
+            if (sides.Length != 3)
+            {
+                throw new ArgumentException("Должно быть передано три стороны для вычисления площади треугольника.");
+            }
+
+            double semiPerimeter = sides.Sum() / 2;
+            double area = Math.Sqrt(semiPerimeter * (semiPerimeter - sides[0]) * (semiPerimeter - sides[1]) * (semiPerimeter - sides[2]));
+            return area;
+        }
+        public double CalculateAreaTriangle(double a, double b, double c)
         {
             double half_p = (a+b+c)/2;
 
@@ -25,6 +35,13 @@
 
             double area = Math.Sqrt((half_p*(half_p-a)*(half_p - b)*(half_p - c)));
             return area;
+        }
+
+        public bool IsRightTriangle(double side1, double side2, double side3)
+        {
+            double[] sides = { side1, side2, side3 };
+            Array.Sort(sides);
+            return Math.Pow(sides[0], 2) + Math.Pow(sides[1], 2) == Math.Pow(sides[2], 2);
         }
     }
 
---
+++ Tests/Tests.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Area_figure;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+
+namespace Area_figure.Tests
+{
+    public class FigureCalculatorTests
+    {
+
+        public void TestCalculateCircleArea()
+        {
+            FigureCalculator calculator = new FigureCalculator();
+            double radius = 5.0;
+            double expectedArea = Math.PI * radius * radius;
+
+            double actualArea = calculator.CalculateAreaCircle(radius);
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        public void TestCalculateTriangleArea()
+        {
+            FigureCalculator calculator = new FigureCalculator();
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 5.0;
+            double expectedArea = 6.0; 
+
+            double actualArea = calculator.CalculateAreaTriangle(side1, side2, side3);
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        public void TestCalculateUnknownShapeArea()
+        {
+            FigureCalculator calculator = new FigureCalculator();
+            double[] sides = { 3.0, 4.0, 5.0 };
+            double expectedArea = 6.0; 
+
+            double actualArea = calculator.CalculateUnknownShapeArea(sides);
+
+            Assert.AreEqual(expectedArea, actualArea);
+        }
+
+        public void TestIsRightTriangle()
+        {
+            FigureCalculator calculator = new FigureCalculator();
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 5.0;
+
+            bool isRightTriangle = calculator.IsRightTriangle(side1, side2, side3);
+
+            Assert.IsTrue(isRightTriangle);
+        }
+
+
+
+
+    }
+
+   
+}
